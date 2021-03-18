c2b476ea46db389ebd1f7987fc13a5840d946560
Gustavo Harff <gustavoharff@insoft4.com>
2021-03-17 21:58:33 -0300

    initial commit

---
+++ src/main/java/api/controllers/UserController.java
@@ -0,0 +1,25 @@
+package api.controllers;
+
+import api.domain.User;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+@Controller
+public class UserController {
+    private static final Gson gson = new Gson();
+
+    @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE, consumes = {"multipart/form-data"})
+    public @ResponseBody
+    ResponseEntity<String> test(int id, String name) throws IOException {
+        User user = new User(id, name);
+
+        return ResponseEntity.status(200).body(gson.toJson(user));
+    }
+}
