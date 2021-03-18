a1a719e1cd50f00026865001989b98e9d7856961
Gustavo Harff <gustavoharff@insoft4.com>
2021-03-17 22:06:16 -0300

    code format

--- src/main/java/api/controllers/UserController.java
+++ src/main/java/api/controllers/UserController.java
@@ -1,25 +1,28 @@
 package api.controllers;
 
 import api.domain.User;
+import com.google.gson.Gson;
+import java.io.IOException;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.Gson;
-
-import java.io.IOException;
-
 @Controller
 public class UserController {
-    private static final Gson gson = new Gson();
 
-    @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE, consumes = {"multipart/form-data"})
-    public @ResponseBody
-    ResponseEntity<String> test(int id, String name) throws IOException {
-        User user = new User(id, name);
+  private static final Gson gson = new Gson();
 
-        return ResponseEntity.status(200).body(gson.toJson(user));
-    }
+  @PostMapping(
+    value = "/users",
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = { "multipart/form-data" }
+  )
+  public @ResponseBody ResponseEntity<String> test(int id, String name)
+    throws IOException {
+    User user = new User(id, name);
+
+    return ResponseEntity.status(200).body(gson.toJson(user));
+  }
 }
--- src/main/java/api/domain/User.java
+++ src/main/java/api/domain/User.java
@@ -1,19 +1,21 @@
 package api.domain;
 
 public class User {
-    private int id;
-    private String name;
 
-    public User(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+  private int id;
 
-    public int getId() {
-        return id;
-    }
+  private String name;
 
-    public String getName() {
-        return name;
-    }
+  public User(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
